aba738978d3184e9e0932be76ef806934d907221
Fonserbc <fonserbc@gmail.com>
2014-01-25 22:33:05 +0100

    Added music

---
+++ test/Assets/Scripts/Music.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+
+	private GameObject cam;
+
+	void Awake() {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+
+	void OnLevelWasLoaded(int level) {
+		cam = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+
+	void Start() {
+		cam = GameObject.FindGameObjectWithTag ("MainCamera");
+		audio.loop = true;
+		audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = cam.transform.position;
+	}
+}
