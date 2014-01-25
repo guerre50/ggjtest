879be489f4ba59caa580a8ef5b78a717b76c3597
Victor Guerrero Corbi <guerre50@gmail.com>
2014-01-25 05:21:39 +0100

    Polymesh example added

---
+++ test/Assets/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+	public float rotationVelocity = 10.0f;
+	private bool carrying = false;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, Time.deltaTime*rotationVelocity, 0, Space.World);
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.transform != transform.parent) {
+			transform.parent = col.gameObject.transform;
+		}
+	}
+
+}
--- test/Assets/Scripts/MyGUI.cs
+++ test/Assets/Scripts/MyGUI.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GUI : MonoBehaviour {
+public class MyGUI : MonoBehaviour {
 	public TextMesh timeText;
 	public TextMesh middleText;
 	public TextMesh lifeText;
