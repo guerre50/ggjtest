2741e65f403ad9b1cbd6a8e5b9199041a7ff4a36
Kindl <janoschkindl@web.de>
2014-01-25 10:38:24 +0100

    different boxes

---
+++ test/Assets/Scripts/Pushable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pushable : MonoBehaviour {
+    public Logic.GameMode movableIn;
+    private Logic _logic;
+
+	// Use this for initialization
+	void Start () {
+        _logic = Logic.instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsMovable())
+        {
+            rigidbody.isKinematic = false;
+        }
+        else
+        {
+            rigidbody.isKinematic = true;
+        }
+	}
+
+    bool IsMovable()
+    {
+        return movableIn == _logic.gameMode;
+    }
+}
