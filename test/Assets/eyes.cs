e3a3b32bd35f15f155f28b61fae701405166fad9
Victor Guerrero Corbi <guerre50@gmail.com>
2014-01-26 07:46:37 +0100

    House on the way

---
+++ test/Assets/eyes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eyes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range(0.0f, 1.0f) > 0.9f) {
+			Blink();
+		}
+		//transform.parent.rigidbody.velocity;
+
+	}
+
+	void Blink() {
+		//iTween.ScaleFrom (eyes, Vector3.zero, 0.1f);
+	}
+}
