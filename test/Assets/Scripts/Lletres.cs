d38d9950349f2ea67eee50dda996abf6d431cc8b
Victor Guerrero Corbi <guerre50@gmail.com>
2014-01-26 14:32:17 +0100

    Scene 002

---
+++ test/Assets/Scripts/Lletres.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lletres : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range (0.0f, 1.0f) < 0.95f) {
+			iTween.PunchPosition(gameObject, new Vector3(Random.Range (-4.0f, 4.0f), 0, Random.Range (-4.0f, 4.0f)), 2.0f);
+		}
+	}
+}
