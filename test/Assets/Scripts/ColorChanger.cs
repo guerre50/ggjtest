e697a1937ce12a59ebcfa0ec0d06c338c03d102a
Fonserbc <fonserbc@gmail.com>
2014-01-26 01:44:31 +0100

    working on color changing

---
+++ test/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChanger : MonoBehaviour {
+
+	private ColorControl _color;
+
+	// Use this for initialization
+	void Start () {
+		_color = ColorControl.instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.color = _color.color;
+	}
+}
---
+++ test/Assets/Scripts/ColorControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorControl : GameObjectSingleton<ColorControl> {
+
+	private Music music;
+	private float bpm = 60.0f;
+	private float colorChange = 20.0f;
+	public Color color;
+
+	private float secondsBetweenBeats;
+	private float lastBeat = 0.0f;
+	private Color nextColor;
+
+	private Vector3 colorVelocity;
+	private Vector3 colorAcc;
+
+	// Use this for initialization
+	void Start () {
+		color = Color.gray;
+		nextColor = Color.gray;
+		music = transform.gameObject.GetComponent<Music> ();
+		secondsBetweenBeats = bpm / 60.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaTime = music.audio.time - lastBeat;
+		if (deltaTime > secondsBetweenBeats) {
+			ChangeColor();
+			lastBeat = music.audio.time;
+		}
+
+
+		color = new Color(color.r +  colorVelocity.x * Time.deltaTime,
+							color.g + colorVelocity.y * Time.deltaTime,
+		                    color.b + colorVelocity.z * Time.deltaTime);
+		colorVelocity += colorAcc * Time.deltaTime;
+		colorAcc = Vector3.Lerp( colorAcc, new Vector3 (0.0f, 0.0f, 0.0f), Time.deltaTime);
+		//Debug.Log (colorAcc);
+	}
+
+	void ChangeColor() {
+		colorAcc = new Vector3 (Random.Range (-colorChange, colorChange), Random.Range (-colorChange, colorChange), Random.Range (-colorChange, colorChange));
+	}
+}
