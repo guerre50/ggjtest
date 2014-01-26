a955ed95981d9ce0fef883af70c90b300994a8f5
Victor Guerrero Corbi <guerre50@gmail.com>
2014-01-26 12:05:49 +0100

    Sun Prefab

--- test/Assets/Scripts/Environment/Sun.cs
+++ test/Assets/Scripts/Environment/Sun.cs
@@ -2,45 +2,28 @@ using UnityEngine;
 using System.Collections;
 
 public class Sun : MonoBehaviour {
-	public GameObject overlay;
-	
-	private Vector3 _overlayTarget;
-	private Vector3 _nightPosition;
-	private Vector3 _dayPosition;
-	private float _overlaySpeed;
-	private Logic _logic;
-	
-	public float inSpeed;
-	public float outSpeed;
-	
-	void Start() {
-		_logic = Logic.instance;
-		//InitOverlay();
+	public GameObject innerSun;
+	public GameObject outerSun;
+
+	public void Start() {
+		iTween.MoveTo (gameObject, iTween.Hash(
+			"position", transform.position + transform.forward*3.0f,
+			"time", 3.0f,
+			"looptype", iTween.LoopType.pingPong
+			));
+
+		/*iTween.ScaleTo (innerSun, iTween.Hash(
+			"scale", innerSun.transform.localScale*1.05f,
+			"time", 2.0f,
+			"easeType", iTween.EaseType.easeInBack,
+			"looptype", iTween.LoopType.pingPong
+			));*/
+
+		_.Wait (0.0f).Done (()=>{iTween.ScaleTo (outerSun, iTween.Hash(
+			"scale", outerSun.transform.localScale*0.95f,
+			"time", 3.0f,
+			"looptype", iTween.LoopType.pingPong
+				));});
 	}
-	
-	
-	void Update () {
-		overlay.transform.position = Vector3.MoveTowards(overlay.transform.position, _overlayTarget, Time.deltaTime*_overlaySpeed);
-	}
-	
-	void InitOverlay() {
-		Vector3 center = Vector3.zero;//worldRect.center;
-		center.z = overlay.transform.position.z;
-		_dayPosition = center;
-		_nightPosition = _dayPosition + Vector3.up*(overlay.transform.localScale.y*10);
-		overlay.transform.position = _nightPosition;
-		_overlayTarget = _nightPosition;
-		overlay.SetActive(true);	
-	}
-	
-	
-	public  void OnSelect() {
-		_overlayTarget = _dayPosition;
-		_overlaySpeed = inSpeed;
-	}
-	
-	public  void OnDeselect() {
-		_overlayTarget = _nightPosition;
-		_overlaySpeed = outSpeed;
-	}
+
 }
