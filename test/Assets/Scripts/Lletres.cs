fe7cb2f2d2ce1cc877a17deb42a8270867954301
Fonserbc <fonserbc@gmail.com>
2014-01-26 16:36:53 +0100

    To upload

--- test/Assets/Scripts/Lletres.cs
+++ test/Assets/Scripts/Lletres.cs
@@ -3,14 +3,28 @@ using System.Collections;
 
 public class Lletres : MonoBehaviour {
 
+	public bool beginText = false;
+
+	private TextMesh t;
+
 	// Use this for initialization
 	void Start () {
+		t = gameObject.GetComponent<TextMesh> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Random.Range (0.0f, 1.0f) < 0.95f) {
-			iTween.PunchPosition(gameObject, new Vector3(Random.Range (-4.0f, 4.0f), 0, Random.Range (-4.0f, 4.0f)), 2.0f);
+		if (beginText) {
+			t.color = new Color(t.color.r, t.color.g, t.color.b, (Mathf.Sin(Time.time/2.0f) + 1.0f)/2.0f);;
 		}
+		else {
+			if (Random.Range (0.0f, 1.0f) < 0.95f) {
+				iTween.PunchPosition (gameObject, new Vector3 (Random.Range (-4.0f, 4.0f), 0, Random.Range (-4.0f, 4.0f)), 2.0f);
+			}
+		}
+	}
+
+	void OnMouseDown() {
+		Application.LoadLevel (1);
 	}
 }
--- test/Assets/Scripts/WinOnTrigger.cs
+++ test/Assets/Scripts/WinOnTrigger.cs
@@ -5,6 +5,8 @@ public class WinOnTrigger : MonoBehaviour {
 
 	void OnTriggerEnter (Collider col) {
 		if (col.gameObject.tag == "Player")
-			Application.LoadLevel ((Application.loadedLevel + 1)%Application.levelCount);
+			if (Application.loadedLevel + 1 == Application.levelCount) 
+					Application.LoadLevel (1);
+			else Application.LoadLevel (Application.loadedLevel + 1);
 	}
 }
